0f8e8c59c9a92270d082b6e16cc2989a3b85a71f
Adamantish <adam.m@hey.com>
2022-09-14 01:25:49 +0200

    WIP: Main page layout tryout

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,66 @@
-typeSpeed = 40
-lineWaitTime = 1700
-var announcement = [
-    "It's great to see you!",
-    "Ah no...relax. We can't actually see you",
-    "But we'd really love to on..."
-]
-secret = 'nobody could ever possibly find this here' // :) Hi!
-
 // ---------- owning tha libs ----------
+
+/*!	
+* FitText.js 1.0 jQuery free version
+*
+* Copyright 2011, Dave Rupert http://daverupert.com 
+* Released under the WTFPL license 
+* http://sam.zoy.org/wtfpl/
+* Modified by Slawomir Kolodziej http://slawekk.info
+*
+* Date: Tue Aug 09 2011 10:45:54 GMT+0200 (CEST)
+*/
+(function(){
+
+    var addEvent = function (el, type, fn) {
+      if (el.addEventListener)
+        el.addEventListener(type, fn, false);
+          else
+              el.attachEvent('on'+type, fn);
+    };
+    
+    var extend = function(obj,ext){
+      for(var key in ext)
+        if(ext.hasOwnProperty(key))
+          obj[key] = ext[key];
+      return obj;
+    };
+  
+    window.fitText = function (el, kompressor, options) {
+  
+      var settings = extend({
+        'minFontSize' : -1/0,
+        'maxFontSize' : 1/0
+      },options);
+  
+      var fit = function (el) {
+        var compressor = kompressor || 1;
+  
+        var resizer = function () {
+          el.style.fontSize = Math.max(Math.min(el.clientWidth / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)) + 'px';
+        };
+  
+        // Call once to set.
+        resizer();
+  
+        // Bind events
+        // If you have any js library which support Events, replace this part
+        // and remove addEvent function (or use original jQuery version)
+        addEvent(window, 'resize', resizer);
+        addEvent(window, 'orientationchange', resizer);
+      };
+  
+      if (el.length)
+        for(var i=0; i<el.length; i++)
+          fit(el[i]);
+      else
+        fit(el);
+  
+      // return set of elements
+      return el;
+    };
+  })();
+
 
 // Half a simple scrambler stolen from https://stackoverflow.com/questions/18279141/javascript-string-encryption-and-decryption/54026460#54026460
 const Unscrambler = salt => {
@@ -51,29 +104,3 @@ function tellItBrother(i=0) {
     if (announcementLine == announcement.length) { return ; }
     setTimeout(function () { return tellItBrother(i); }, expectedLineTime + lineWaitTime)
 }
-
-// ---------- Script ---------------
-var el = document.getElementById("typeSpace")
-var announcementLine = 0
-
-const unscrambler = Unscrambler(secret)
-
-var scrambled_person = params().e
-var person
-
-if (scrambled_person) {
-    person = unscrambler(params().e)
-} else {
-    person = "Hey there" // Somehow we didn't get the param so fallback
-}
-
-announcement.unshift(person)
-
-tellItBrother()
-
-// var scriptReadingInterval = setInterval(function () { 
-//     typeLine()
-//     if (announcementLine == announcement.length) { clearInterval(scriptReadingInterval) };
-//  }, scrollSpeed)
-
- 
